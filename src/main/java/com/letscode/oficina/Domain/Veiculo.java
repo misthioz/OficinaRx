aa132e19eeb4e9bc9c7c4ea6ae90c02463756610
ajurberg <ajurberg@gmail.com>
2021-09-05 11:08:53 -0300

    Criação da classe Veículo

---
+++ src/main/java/com/letscode/oficina/Domain/Veiculo.java
@@ -0,0 +1,25 @@
+package com.letscode.oficina.Domain;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Veiculo {
+
+    @SerializedName("carPlate")
+    private String placa;
+
+    @SerializedName("carBrand")
+    private String marca;
+
+    @SerializedName("carColor")
+    private String cor;
+
+    @SerializedName("client")
+    private Cliente cliente;
+
+}
