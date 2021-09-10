57baba137a421413ec725522fb16191207f280d0
ajurberg <ajurberg@gmail.com>
2021-09-09 22:13:12 -0300

    Atualização da classe MecanicoService (em andamento)

--- src/main/java/com/letscode/oficina/service/MecanicoService.java
+++ src/main/java/com/letscode/oficina/service/MecanicoService.java
@@ -3,7 +3,6 @@ package com.letscode.oficina.service;
 import com.letscode.oficina.domain.Especialidade;
 import com.letscode.oficina.domain.Mecanico;
 import com.letscode.oficina.repository.MecanicoRepository;
-import com.letscode.oficina.request.MecanicoRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -15,27 +14,29 @@ public class MecanicoService {
 
     private final MecanicoRepository mecanicoRepository;
 
+    public Mono<Mecanico> save(Mecanico mecanico) {
+        // TODO
+        return null;
+    }
+
     public Flux<Mecanico> listAll() {
         return mecanicoRepository.findAll();
     }
 
-    public Mono<Mecanico> save(Mecanico mecanico) {
-        return null; //mecanico.mapflatMap(mecanicoRepository::insert);
+    public Flux<Mecanico> findBySpeciality(Especialidade especialidade) {
+        return mecanicoRepository.findMecanicosByEspecialidade(especialidade);
     }
 
     public Mono<Mecanico> findByName(String name) {
-        return null;
+        return mecanicoRepository.findMecanicoByName(name);
     }
 
     public Mono<Mecanico> findById(String idMecanico) {
-        return null;
+        return mecanicoRepository.findById(idMecanico);
     }
 
     public Mono<Void> deleteById(String idMecanico) {
-        return null;
+        return mecanicoRepository.deleteById(idMecanico);
     }
 
-    public Flux<Mecanico> findBySpeciality(Especialidade especialidade) {
-        return null;
-    }
 }
