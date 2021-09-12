9f65330f7eec591dd481c81fa510381a6c9cce0f
desiandrade <desi.andrade@hotmail.com>
2021-09-12 14:26:51 -0300

    Limpando o código não utilizado

--- src/main/java/com/letscode/oficina/response/ClienteResponse.java
+++ src/main/java/com/letscode/oficina/response/ClienteResponse.java
@@ -3,11 +3,6 @@ package com.letscode.oficina.response;
 import com.letscode.oficina.domain.Cliente;
 import com.letscode.oficina.domain.Endereco;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -21,19 +16,5 @@ public class ClienteResponse {
     private String enderecoNumero;
     private Endereco endereco;
     private List<TelefoneClienteResponseMinimal> telefones;
-
-//    public ClienteResponse(Endereco endereco, Cliente cliente) {
-//        this.nome = cliente.getNome();
-//        this.endereco = endereco;
-//    }
-
-    public static ClienteResponse convert (Cliente cliente){
-        ClienteResponse clienteResponse = new ClienteResponse();
-        clienteResponse.setNome(cliente.getNome());
-        clienteResponse.setId(cliente.getId());
-
-
-        return clienteResponse;
-    }
 
 }
